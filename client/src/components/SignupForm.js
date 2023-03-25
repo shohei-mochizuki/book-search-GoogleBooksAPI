fa532b1f43a34baa51341045c48f6667f7c76fbb
shohei-mochizuki <121307266+shohei-mochizuki@users.noreply.github.com>
2023-03-24 20:19:08 -0400

    Delete old code using RESTful API in SignupForm

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -8,7 +8,6 @@ import { useMutation } from '@apollo/client';
 // Import queries and mutations from '../utils/queries';
 import { CREATE_USER } from '../utils/mutations';
 
-// import { createUser } from '../utils/API';
 import Auth from '../utils/auth';
 
 const SignupForm = () => {
@@ -41,27 +40,11 @@ const SignupForm = () => {
       event.stopPropagation();
     }
 
-    try {
-      
+    try {      
       const { token, user } = await createUser({
         variables: { ...userFormData }, // OR simply userFormData
       });
       
-
-
-      
-      const response = await createUser(userFormData);
-
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
-
-      const { token, user } = await response.json();    
-      
-      
-      
-      
-
       console.log(user);
       Auth.login(token);
     } catch (err) {
