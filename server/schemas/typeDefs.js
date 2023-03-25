d36351d66e6d42aae6c694b8d7e13ad96cbd5488
shohei-mochizuki <121307266+shohei-mochizuki@users.noreply.github.com>
2023-03-25 13:53:06 -0400

    Create types in typeDefs.js

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -6,7 +6,7 @@ const typeDefs = gql`
     username: String!
     email: String!
     password: String!
-    savedBooks: [Book]
+    savedBooks: [Book]!
   }
 
   type Book {
@@ -15,21 +15,23 @@ const typeDefs = gql`
     bookId: String!
     image: String
     link: String
-    authors: [Author]
+    authors: [String]!
   }
 
-  type Author {
-    name: String
+  type Auth {
+    token: ID!
+    user: User
   }
 
   type Query {
-    tech: [Tech]
-    matchups(_id: String): [Matchup]
+    me: User
   }
 
   type Mutation {
-    createMatchup(tech1: String!, tech2: String!): Matchup
-    createVote(_id: String!, techNum: Int!): Matchup
+    createUser(username: String!, email: String!, password: String!): Auth
+    login(email: String!, password: String!): Auth
+    saveBook(_id: ID!, description: String!, bookId: String!, image: String, link: String, authors: [String]!): User
+    deleteBook(_id: ID!, bookId: String!): User
   }
 `;
 
